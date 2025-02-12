11d3c41522d5611c1e6b7e92f51a32fda6d4577b
devaharish2004 <devaharishm@gmail.com>
2025-02-12 20:22:44 +0530

    Pascal's Triangle

---
+++ Arrays/Pascal'sTriangle/Solution.cpp
@@ -0,0 +1,201 @@
+/*
+Variation 1 : Find an element at rth row and cth col
+*/
+
+/*
+Brute force (Naive) : Calculate using nCr formula = n!/((r)!*(n-r)!) - (r-1)C(c-1)
+
+TC : O(N) + O(R) + O(N-R)
+*/
+
+//Optimal approach :
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
+
+int main()
+{
+    int r = 5; // row number
+    int c = 3; // col number
+    int element = pascalTriangle(r, c);
+    cout << "The element at position (r,c) is: "
+            << element << "n";
+    return 0;
+}
+
+//TC : O(r) - since we are iterating for r times (here c)
+        
+        
+
+/*
+Variation 2 : Finding all elements of a row in the triangle
+*/
+
+// Naive approach using the previous approach in a loop
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+void pascalTriangle(int n) {
+    // printing the entire row n:
+    for (int c = 1; c <= n; c++) {
+        cout << nCr(n - 1, c - 1) << " ";
+    }
+    cout << "n";
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
+
+// TC : O(N^2)
+
+// Optimal approach - multiplying new elements with previously computed values in the row
+#include <bits/stdc++.h>
+using namespace std;
+
+void pascalTriangle(int n) {
+    long long ans = 1;
+    cout << ans << " "; // printing 1st element
+
+    //Printing the rest of the part:
+    for (int i = 1; i < n; i++) {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
+// TC: O(N)
+
+/*
+Variation 3 - Print the pascal triangle till nth row
+*/
+
+// Brute force :
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
+
+// TC: O(N^3)
+
+//Optimal approach :
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> generateRow(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
+
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    //store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
+
+//TC : O(N^2)
